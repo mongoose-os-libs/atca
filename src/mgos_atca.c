d072ede8f74972673e28fc1ef32b9d1297f6e1b2
Deomid Ryabkov <rojer@cesanta.com>
2019-01-09 19:16:37 +0000

    Move atcac_sw_sha2_256 to common code
    CL: none
    
    PUBLISHED_FROM=4727c40f339beb02d4eb4e458c628cd4ca234731

--- src/mgos_atca.c
+++ src/mgos_atca.c
@@ -23,6 +23,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "mbedtls/sha256.h"
+
 #include "common/cs_dbg.h"
 
 #include "mongoose.h"
@@ -279,4 +281,10 @@ void atca_delay_ms(uint32_t delay) {
 
 void atca_delay_us(uint32_t delay) {
   mgos_usleep(delay);
+}
+
+int atcac_sw_sha2_256(const uint8_t *data, size_t data_size,
+                      uint8_t digest[32]) {
+  mbedtls_sha256(data, data_size, digest, false /* is_224 */);
+  return 0;
 }
